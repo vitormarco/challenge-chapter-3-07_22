d2d26527b064a9d0b6feb3d10578acd258c26a66
Vítor Marco <vitoradrianomarco@gmail.com>
2022-07-04 08:28:14 -0300

    Config prismic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
+import { PrismicPreview } from '@prismicio/next';
+import { PrismicProvider } from '@prismicio/react';
 import { AppProps } from 'next/app';
+import Link from 'next/link';
+import { linkResolver, repositoryName } from '../services/prismic';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <PrismicProvider
+      linkResolver={linkResolver}
+      internalLinkComponent={({ href, children, ...props }) => (
+        <Link href={href}>
+          <a {...props}>{children}</a>
+        </Link>
+      )}
+    >
+      <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
+  );
 }
 
 export default MyApp;
--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,10 +1,14 @@
 import * as prismic from '@prismicio/client';
 import { HttpRequestLike } from '@prismicio/client';
 import { enableAutoPreviews } from '@prismicio/next';
+import sm from 'sm.json';
 
 export interface PrismicConfig {
   req?: HttpRequestLike;
 }
+
+export const endpoint = sm.apiEndpoint;
+export const repositoryName = prismic.getRepositoryName(endpoint);
 
 export function getPrismicClient(config: PrismicConfig): prismic.Client {
   const client = prismic.createClient(process.env.PRISMIC_API_ENDPOINT);
@@ -12,7 +16,18 @@ export function getPrismicClient(config: PrismicConfig): prismic.Client {
   enableAutoPreviews({
     client,
     req: config.req,
-  })
+  });
 
   return client;
+}
+
+export function linkResolver(doc) {
+  switch (doc.type) {
+    case 'homepage':
+      return '/';
+    case 'page':
+      return `/${doc.uid}`;
+    default:
+      return null;
+  }
 }
